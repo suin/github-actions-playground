eef26a2766b1e7093af1f741646543402a4e0528
suin <suinyeze@gmail.com>
2020-05-19 13:28:30 +0900

    feat: 🎸 generate typedoc and deploy to GitHub Pages

---
+++ README.md
@@ -0,0 +1,16 @@
+# foo
+
+An Awesome library.
+
+## Installation
+
+```
+npm install ...
+```
+
+## Basic usage
+
+```js
+import {Foo} from 'foo'
+const foo = new Foo()
+```
---
+++ src/object.ts
@@ -0,0 +1,31 @@
+export interface Object {
+  /** The initial value of Object.prototype.constructor is the standard built-in Object constructor. */
+  constructor: Function;
+
+  /** Returns a string representation of an object. */
+  toString(): string;
+
+  /** Returns a date converted to a string using the current locale. */
+  toLocaleString(): string;
+
+  /** Returns the primitive value of the specified object. */
+  valueOf(): Object;
+
+  /**
+   * Determines whether an object has a property with the specified name.
+   * @param v A property name.
+   */
+  hasOwnProperty(v: PropertyKey): boolean;
+
+  /**
+   * Determines whether an object exists in another object's prototype chain.
+   * @param v Another object whose prototype chain is to be checked.
+   */
+  isPrototypeOf(v: Object): boolean;
+
+  /**
+   * Determines whether a specified property is enumerable.
+   * @param v A property name.
+   */
+  propertyIsEnumerable(v: PropertyKey): boolean;
+}
